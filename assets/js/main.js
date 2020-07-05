711507071d6b59010a76374add2728efe35d2d12
emanmoq <42995029+emanmoq@users.noreply.github.com>
2020-07-05 13:06:24 +0300

    dd

--- assets/js/main.js
+++ assets/js/main.js
@@ -118,8 +118,23 @@ jQuery(function ($) {
                   }
               });
             },1000);
-
-          
+            $('.ourTeamCar').owlCarousel({
+              autoplay:true,
+              loop:true,
+              margin:17,
+            responsive:{
+            0:{
+              items:1
+            },
+            600:{
+              items:2
+            },
+            1000:{
+              items:4
+            }
+            }
+            });
+            
    
      
             $(".close").click(function(){
