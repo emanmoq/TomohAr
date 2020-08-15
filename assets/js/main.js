02fa426d1fdb1cd97294282ab13e45052e847a72
emanmoq <42995029+emanmoq@users.noreply.github.com>
2020-08-16 01:35:01 +0300

    gg

--- assets/js/main.js
+++ assets/js/main.js
@@ -134,7 +134,23 @@ jQuery(function ($) {
             }
             }
             });
-            
+            $('.newsCar').owlCarousel({
+              autoplay:true,
+              loop:true,
+             margin:17,
+            responsive:{
+            0:{
+              items:1
+            },
+            600:{
+              items:2
+            },
+            1000:{
+              items:4
+            }
+            }
+
+            });
    
      
             $(".close").click(function(){
