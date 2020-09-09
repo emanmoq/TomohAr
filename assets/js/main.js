926e4f75ead66c9142d565c5f7eab998723b5a63
emanmoq <42995029+emanmoq@users.noreply.github.com>
2020-09-09 11:11:09 +0300

    ss

--- assets/js/main.js
+++ assets/js/main.js
@@ -163,7 +163,7 @@ jQuery(function ($) {
               },
               1000:{
                 nav:false,
-                items:2
+                items:4
               }
               ,
               1300:{
